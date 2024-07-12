812fd1375180b720f57e6e6ad4a30ed7edc390b0
christopherkiprop <choppachepa@gmail.com>
2024-07-12 23:29:40 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  function appendCat(name) {
+    return [...cats, name];
+  }
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+   
